4742b76bcb87eb08e8dab871a108ab18f86e70d2
silveira <silveiraneto@gmail.com>
2016-03-25 14:56:27 -0400

    adding new packages

--- bin/ubuntu_unboxing.sh
+++ bin/ubuntu_unboxing.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-pkgs=(git python-pip xchat gimp inkscape blender sshfs synaptic unrar compizconfig-settings-manager audacity)
+pkgs=(git python-pip xchat gimp inkscape blender sshfs synaptic unrar compizconfig-settings-manager audacity p7zip-full)
+
 
 # install packages
 for package in ${pkgs[@]};
@@ -15,4 +16,4 @@ sudo pip install awscli
 gsettings set com.canonical.desktop.interface scrollbar-mode normal
 
 # remove lens-shopping (amazon.com stuff on Ubuntu)
-sudo apt-get remove unity-lens-shopping
+sudo apt-get remove unity-scope-home
