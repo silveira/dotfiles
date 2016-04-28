3ab26f5ae9e799d045252087f072659ec8878c59
Silveira Neto <jmsn@ufc3fdb42e54c5702861b.ant.amazon.com>
2016-04-28 11:56:59 -0400

    feature. adding gedit autosave and lynx command line browser.

--- bin/ubuntu_unboxing.sh
+++ bin/ubuntu_unboxing.sh
@@ -13,6 +13,7 @@ pkgs=(
 	compizconfig-settings-manager
 	audacity
 	p7zip-full
+	lynx
 )
 
 
@@ -31,3 +32,7 @@ gsettings set com.canonical.desktop.interface scrollbar-mode normal
 
 # remove lens-shopping (amazon.com stuff on Ubuntu)
 sudo apt-get remove unity-scope-home
+
+# gedit autosave 1 minute
+gsettings set org.gnome.gedit.preferences.editor auto-save true
+gsettings set org.gnome.gedit.preferences.editor auto-save-interval 1
